0f1fa964d192bd70e3cbc439f3732e20c52a87ff
Wu, Chunyuan <chunyuan.wu@intel.com>
2023-06-30 09:50:39 +0800

    enable dynamic quant for lm.transformer and lm.linears

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 
+import time
 import os
 os.environ['CURL_CA_BUNDLE'] = ''
+
+import torch
 
 import torchaudio
 from audiocraft.models import MusicGen
@@ -8,11 +11,20 @@ from audiocraft.data.audio import audio_write
 
 model = MusicGen.get_pretrained('melody')
 model.set_generation_params(duration=30)  # generate 30 seconds.
+
+model.lm.transformer = torch.quantization.quantize_dynamic(
+    model.lm.transformer, {torch.nn.Linear}, dtype=torch.qint8, inplace=True
+)
+model.lm.linears = torch.quantization.quantize_dynamic(
+    model.lm.linears, {torch.nn.Linear}, dtype=torch.qint8, inplace=True
+)
+
 #wav = model.generate_unconditional(4)    # generates 4 unconditional audio samples
 #descriptions = ['happy rock', 'energetic EDM', 'sad jazz']
 descriptions = ['happy rock']
+t0 = time.time()
 wav = model.generate(descriptions)  # generates 3 samples. 1 text to music
-
+print("generate time: ", time.time() - t0)
 #melody, sr = torchaudio.load('./assets/bach.mp3') # text+melody to music
 # generates using the melody from the given audio and the provided descriptions.
 #wav = model.generate_with_chroma(descriptions, melody[None].expand(3, -1, -1), sr) # text+melody to music
